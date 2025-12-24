8a15339f2c6f0e591ca6486e36155314f19046fd
leos <leos@primatrix.ai>
2025-12-24 16:03:46 +0800

    fix

--- python/sgl_jax/srt/models/bailing_moe.py
+++ python/sgl_jax/srt/models/bailing_moe.py
@@ -158,7 +158,6 @@ class BailingMoEMLP(nnx.Module):
         self.gate_proj = LinearBase(
             input_size=hidden_size,
             output_size=intermediate_size,
-            kernel_axes=(None, "tensor"),
             use_bias=False,
             params_dtype=dtype,
             mesh=mesh,
@@ -167,7 +166,6 @@ class BailingMoEMLP(nnx.Module):
         self.up_proj = LinearBase(
             input_size=hidden_size,
             output_size=intermediate_size,
-            kernel_axes=(None, "tensor"),
             use_bias=False,
             params_dtype=dtype,
             mesh=mesh,
@@ -176,7 +174,6 @@ class BailingMoEMLP(nnx.Module):
         self.down_proj = LinearBase(
             input_size=intermediate_size,
             output_size=hidden_size,
-            kernel_axes=("tensor", None),
             use_bias=False,
             params_dtype=dtype,
             mesh=mesh,
