bd11cb33c4f1fdf110aaeca61afab04e7ffb117a
leos <leos@primatrix.ai>
2026-01-05 16:24:41 +0800

    feat: tune RPAv3 block spec for tpuv7

--- benchmark/kernels/flash_attention/bench_flashattention.py
+++ benchmark/kernels/flash_attention/bench_flashattention.py
@@ -16,6 +16,9 @@ from sgl_jax.srt.kernels.ragged_paged_attention.ragged_paged_attention import (
     get_kernel_scope_name,
     ragged_paged_attention,
 )
+from sgl_jax.srt.kernels.ragged_paged_attention.tuned_block_sizes import (
+    get_tuned_block_sizes,
+)
 from sgl_jax.srt.kernels.utils.perf import multiple_iteration_timeit_from_trace
 from sgl_jax.test.test_utils import CustomTestCase, is_in_ci
 
@@ -131,11 +134,22 @@ def benchmark_backend(
     jax.block_until_ready(output)
 
     # Benchmark
+    best_bkv_p, best_bq_sz = get_tuned_block_sizes(
+        q.dtype,
+        k.dtype,
+        q_head_num,
+        kv_head_num,
+        head_dim,
+        page_size,
+        max_num_batched_tokens,
+        page_indices.shape[0] // kv_lens.shape[0],
+        True,
+    )
     times = multiple_iteration_timeit_from_trace(
         compute_func=lambda: attn(),
         data_generator=lambda: (),
-        task=get_kernel_scope_name(16, 2, page_size),
-        tries=3,
+        task=get_kernel_scope_name(best_bq_sz, best_bkv_p, page_size),
+        tries=1,
     )
     avg_time = float(np.mean(times)) if times else float("nan")
 
@@ -241,22 +255,38 @@ class TestPerformance(CustomTestCase):
         # Key: (mode, page_size, max_num_batched_tokens, q_head_num, kv_head_num, head_dim, max_kv_cache_tokens)
         # Value: expected cost-time (baseline) in ms
         test_cases = {
-            ("prefill", 128, 1024, 4, 4, 128, 600000): 0.20109666666666667,
-            ("prefill", 128, 1024, 4, 2, 128, 600000): 0.13419916666666667,
-            ("prefill", 128, 1024, 8, 8, 128, 600000): 0.33863875000000004,
-            ("prefill", 128, 1024, 8, 4, 128, 600000): 0.20110458333333334,
-            ("prefill", 128, 4096, 4, 4, 128, 600000): 1.5589241666666667,
-            ("prefill", 128, 4096, 4, 2, 128, 600000): 1.0281283333333333,
-            ("prefill", 128, 4096, 8, 8, 128, 600000): 2.65723875,
-            ("prefill", 128, 4096, 8, 4, 128, 600000): 1.5589158333333335,
-            ("decode", 128, 128, 4, 4, 128, 600000): 0.7600533333333334,
-            ("decode", 128, 128, 4, 2, 128, 600000): 0.5717766666666667,
-            ("decode", 128, 128, 8, 8, 128, 600000): 1.16352,
-            ("decode", 128, 128, 8, 4, 128, 600000): 0.7622062500000001,
-            ("decode", 128, 256, 4, 4, 128, 600000): 1.492135,
-            ("decode", 128, 256, 4, 2, 128, 600000): 1.130425,
-            ("decode", 128, 256, 8, 8, 128, 600000): 2.2918925,
-            ("decode", 128, 256, 8, 4, 128, 600000): 1.4930079166666665,
+            ("prefill", 128, 1024, 4, 1, 128, 600000): 0.0158863,
+            ("prefill", 128, 1024, 4, 2, 128, 600000): 0.0150987,
+            ("prefill", 128, 1024, 8, 1, 128, 600000): 0.0217812,
+            ("prefill", 128, 1024, 8, 4, 128, 600000): 0.0241038,
+            ("prefill", 128, 4096, 4, 1, 128, 600000): 0.091245,
+            ("prefill", 128, 4096, 4, 2, 128, 600000): 0.091995,
+            ("prefill", 128, 4096, 8, 1, 128, 600000): 0.147034,
+            ("prefill", 128, 4096, 8, 4, 128, 600000): 0.161692,
+            ("prefill", 256, 1024, 4, 1, 128, 600000): 0.01573,
+            ("prefill", 256, 1024, 4, 2, 128, 600000): 0.015095,
+            ("prefill", 256, 1024, 8, 1, 128, 600000): 0.021755,
+            ("prefill", 256, 1024, 8, 4, 128, 600000): 0.0237662,
+            ("prefill", 256, 4096, 4, 1, 128, 600000): 0.091365,
+            ("prefill", 256, 4096, 4, 2, 128, 600000): 0.0914912,
+            ("prefill", 256, 4096, 8, 1, 128, 600000): 0.146874,
+            ("prefill", 256, 4096, 8, 4, 128, 600000): 0.161585,
+            ("decode", 128, 128, 4, 1, 128, 600000): 0.235763,
+            ("decode", 128, 128, 4, 2, 128, 600000): 0.303399,
+            ("decode", 128, 128, 8, 1, 128, 600000): 0.239314,
+            ("decode", 128, 128, 8, 4, 128, 600000): 0.529945,
+            ("decode", 128, 256, 4, 1, 128, 600000): 0.461838,
+            ("decode", 128, 256, 4, 2, 128, 600000): 0.590747,
+            ("decode", 128, 256, 8, 1, 128, 600000): 0.456383,
+            ("decode", 128, 256, 8, 4, 128, 600000): 1.06428,
+            ("decode", 256, 128, 4, 1, 128, 600000): 0.167162,
+            ("decode", 256, 128, 4, 2, 128, 600000): 0.26077,
+            ("decode", 256, 128, 8, 1, 128, 600000): 0.168829,
+            ("decode", 256, 128, 8, 4, 128, 600000): 0.405069,
+            ("decode", 256, 256, 4, 1, 128, 600000): 0.331566,
+            ("decode", 256, 256, 4, 2, 128, 600000): 0.504135,
+            ("decode", 256, 256, 8, 1, 128, 600000): 0.331131,
+            ("decode", 256, 256, 8, 4, 128, 600000): 0.809101,
         }
         max_context_len = 40960
         for case, baseline in test_cases.items():
