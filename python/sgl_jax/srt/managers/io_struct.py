5527cecca6005ea3555ae89cc416f1f6a3294c92
pathfinder-fp <slackexplorer@gmail.com>
2026-01-06 15:22:23 +0800

    add return hidden_state

--- python/sgl_jax/srt/managers/io_struct.py
+++ python/sgl_jax/srt/managers/io_struct.py
@@ -129,6 +129,8 @@ class TokenizedGenerateReqInput:
     lora_id: str | None = None  # None means just use the base model
     # Extra key for cache namespace isolation (e.g., cache_salt, lora_id)
     extra_key: str | None = None
+    # whether to return hidden states
+    return_hidden_states: bool = False
 
 
 @dataclass
