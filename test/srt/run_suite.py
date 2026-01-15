938e0549b4e0380dd191311227909119d61ae45f
neo <pengchengneo@gmail.com>
2026-01-15 15:49:38 +0800

    add test/srt/test_server_pause_continue.py test time

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -454,7 +454,7 @@ suites = {
         TestFile("test/srt/test_engine_determine_generation.py", 3),
         TestFile("test/srt/test_engine_flush_cache.py", 3),
         TestFile("test/srt/test_engine_pause_continue.py", 3),
-        TestFile("test/srt/test_server_pause_continue.py", 6),
+        TestFile("test/srt/test_server_pause_continue.py", 10),
         TestFile("test/srt/multimodal/test_wan2_1_models.py", 30),
     ],
 }
