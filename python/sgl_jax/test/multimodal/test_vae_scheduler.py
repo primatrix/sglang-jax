c3d3f279a0b7ae5640f7dda8d4a74a604b4b1025
whz <wuhuanzi@wuhuanziMacBook-Air.local>
2026-01-16 14:44:06 +0800

    fix

--- python/sgl_jax/test/multimodal/test_vae_scheduler.py
+++ python/sgl_jax/test/multimodal/test_vae_scheduler.py
@@ -17,8 +17,9 @@ class TestVaeScheduler(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.mesh = jax.sharding.Mesh(jax.devices(), axis_names=("data",))
         cls.server_args = MultimodalServerArgs(
-            model_path="Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
+            model_path="/models/Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
             download_dir="/dev/shm",
         )
         cls.backend = QueueBackend(in_queue=queue.Queue(), out_queue=queue.Queue())
