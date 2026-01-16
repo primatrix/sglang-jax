2bd336ab515b2b636ef93f5c149937c9bbc03730
whz <wuhuanzi@wuhuanziMacBook-Air.local>
2026-01-16 13:50:56 +0800

    fix

--- python/sgl_jax/test/multimodal/test_wan_vae_precision.py
+++ python/sgl_jax/test/multimodal/test_wan_vae_precision.py
@@ -13,6 +13,7 @@ from sgl_jax.srt.multimodal.models.wan2_1.vaes.wanvae import AutoencoderKLWan as
 import torch
 from diffusers import AutoencoderKLWan
 from sgl_jax.srt.multimodal.configs.vaes.wan_vae_config import WanVAEConfig
+from sgl_jax.srt.configs.model_config import ModelConfig
 
 class TestWanVaePrecision(unittest.TestCase):
     """Test VaeScheduler full load and forward flow."""
@@ -31,9 +32,9 @@ class TestWanVaePrecision(unittest.TestCase):
         cls.vae.eval()
 
         cls.jax_vae = JaxWan(WanVAEConfig(), mesh=cls.mesh)
-        cls.jax_vae.load_weights(WanVAEConfig())
+        cls.jax_vae.load_weights(model_config=ModelConfig(model_path=cls.server_args.model_path + "/vae"))
 
-    def _get_transformer_encode_output(self):
+    def _get_diffusers_encode_output(self):
         input = (
             torch.tensor(np.arange(1 * 5 * 192 * 192 * 3), dtype=torch.float32)
             .reshape(1, 5, 192, 192, 3)
@@ -41,9 +42,10 @@ class TestWanVaePrecision(unittest.TestCase):
         )
         latents = self.vae.encode(input)
         print(latents.latent_dist.parameters.shape)
-        return latents.latent_dist.parameters.detach().numpy()
-        with open("test_encode_output.npy", "wb") as f:
+        with open("wan_vae_diffusers_encode_output.npy", "wb") as f:
             np.save(f, latents.latent_dist.parameters.detach().numpy())
+
+        return latents.latent_dist.parameters.detach().numpy()
     
     def _get_jax_encode_output(self,):
         input = jnp.array(np.arange(1 * 5 * 192 * 192 * 3), dtype=jnp.float32).reshape(1, 5, 192, 192, 3)
@@ -59,7 +61,10 @@ class TestWanVaePrecision(unittest.TestCase):
         )
         y = self.vae.decode(latents)
         print(y.sample.shape)
+        with open("wan_vae_diffusers_decode_output.npy", "wb") as f:
+            np.save(f, y.sample.detach().numpy())
         return y.sample.detach().numpy()
+    
     def _get_jax_decode_output(self):
         latents = jnp.array(np.arange(1 * 5 * 3 * 4 * 16), dtype=jnp.float32).reshape(1, 5, 3, 4, 16)
         y = self.jax_vae.decode(latents)
