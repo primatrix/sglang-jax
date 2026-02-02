2feab35ad3702042b7db5a99d72b9e4154f4241d
prayer <prayer@primatrix.ai>
2026-02-02 21:58:30 +0800

    add saver

--- python/sgl_jax/srt/models/bailing_moe.py
+++ python/sgl_jax/srt/models/bailing_moe.py
@@ -393,19 +393,16 @@ class BailingMoEDecoderLayer(nnx.Module):
             hidden_states = self.mlp(hidden_states)
             topk_ids = None
 
-        with jax.set_mesh(self.mesh):
-            global_replicated_sharding = jax.sharding.NamedSharding(
-                self.mesh, jax.sharding.PartitionSpec(None, None)
-            )
+        global_replicated_sharding = jax.sharding.NamedSharding(
+            self.mesh, jax.sharding.PartitionSpec(None, None)
+        )
 
-            # 将数据强制 Reshard/Constraint 到全局 Replicated 状态
-            # 这一步会触发必要的 AllGather 通信，将切片数据聚合
-            hidden_states_safe = jax.lax.with_sharding_constraint(
-                hidden_states, global_replicated_sharding
-            )
+        # 将数据强制 Reshard/Constraint 到全局 Replicated 状态
+        # 这一步会触发必要的 AllGather 通信，将切片数据聚合
+        hidden_states_safe = jax.sharding.reshard(hidden_states, global_replicated_sharding)
 
-            # 现在可以安全地调用 io_callback，因为数据和环境都在主 Mesh 上
-            io_callback(_save_moe_output_impl, None, hidden_states_safe, self.layer_id)
+        # 现在可以安全地调用 io_callback，因为数据和环境都在主 Mesh 上
+        io_callback(_save_moe_output_impl, None, hidden_states_safe, self.layer_id)
 
         return hidden_states, residual, kv_fused, topk_ids
 
